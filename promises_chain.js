fd06e12ca5181cde9a2e1e7fc45ae3304ecdcbd5
nehakhatriya <nehakhatriya@gmail.com>
2020-08-21 16:56:15 +0530

    promise chains

---
+++ promises_chain.js
@@ -0,0 +1,25 @@
+let cleanRoom= function(){
+    return new Promise(function(resolve,reject){
+        resolve(" cleaned room ")
+    })
+}
+
+let removeGarbage=function(msg){
+    return new Promise(function(resolve,reject){
+    resolve(msg+" removed garbage ")
+    })
+}
+
+let iceCream=function(msg){
+  return new Promise(function(resolve,reject){
+      return resolve(msg +" can have ice cream ")
+  })
+}
+
+cleanRoom().then(function(result){
+    return removeGarbage(result)
+}).then(function(result){
+    return iceCream(result)
+}).then (function(result){
+    console.log("finished"+result)
+})
