a60b3721dd34f7715671b6da26261032166a5b1c
nehakhatriya <nehakhatriya@gmail.com>
2020-09-02 15:13:28 +0530

    removed promise-logger.js file

--- logging/promise-logger.js
+++
@@ -1,8 +0,0 @@
-const { createLogger, format, transports}= require('winston')
-
-const logger= createLogger({
-    level: "info",
-    format: format.combine(format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss'}),format.json()),
-    transports:[ new transports.File({filename:'./logging/promise-log.log'})]
-})
-module.exports= logger
